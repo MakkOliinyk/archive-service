d1b4e8a32fad02275aa2b41effb559791c0491aa
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 16:11:51 +0300

    Add archiving and unarchiving logic

---
+++ utils/index.js
@@ -0,0 +1,49 @@
+import { createGzip, createGunzip } from 'zlib';
+
+const archiveFile = (fileContent) => {
+    const gzip = createGzip();
+
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+
+        gzip.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        gzip.on('end', () => {
+            const compressedContent = Buffer.concat(chunks);
+            resolve(compressedContent);
+        });
+
+        gzip.on('error', (error) => {
+            reject(error);
+        });
+
+        gzip.write(fileContent);
+        gzip.end();
+    });
+};
+
+const unarchiveFile = (fileContent) => {
+    const gunzip = createGunzip();
+
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+
+        gunzip.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        gunzip.on('end', () => {
+            const decompressedContent = Buffer.concat(chunks);
+            resolve(decompressedContent);
+        });
+
+        gunzip.on('error', (error) => {
+            reject(error);
+        });
+
+        gunzip.write(fileContent);
+        gunzip.end();
+    });
+};
