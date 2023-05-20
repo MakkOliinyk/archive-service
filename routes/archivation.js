b38c08ff56aaf23f9a0e25ae8b292294b5a2e22f
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 16:11:59 +0300

    Add routes

---
+++ routes/archivation.js
@@ -0,0 +1,23 @@
+const fileRoutes = async (server, options) => {
+    server.route({
+        method: 'POST',
+        path: '/archive',
+        handler: async (request, reply) => {
+            const fileContent = request.body.file;
+            const compressedContent = await archiveFile(fileContent);
+            reply.type('application/octet-stream').send(compressedContent);
+        },
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/unarchive',
+        handler: async (request, reply) => {
+            const fileContent = request.body.file;
+            const decompressedContent = await unarchiveFile(fileContent);
+            reply.type('application/octet-stream').send(decompressedContent);
+        },
+    });
+};
+
+export default fastifyPlugin(fileRoutes);
